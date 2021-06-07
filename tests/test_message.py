c7ec919ec5cd6d57ba54aefede4502b9e79b1fc7
Armaan Ahmed <aahmed22@scarsdaleschools.org>
2021-06-07 00:16:08 -0400

    Added test case for sending function specific output

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,1 +1,11 @@
-completemessage = __import__("src/complete_message")
+from taskmessenger.send import CompleteMessage,EmailData
+
+c = CompleteMessage("user@example.com") #Use specified email with the default optional arguments
+p = input() #Unsafely get password
+c.set_passwd(p) #Set the one-time password 
+
+@c.send_function_specific_email #Send email message based on the second element of the tuple
+def a(i): #Function will return i**2 and send as an email [Subject: i, Body: i**2]
+	return (i**2,EmailData(str(i),str(i**2))) 
+
+print(a(66)) #4356
