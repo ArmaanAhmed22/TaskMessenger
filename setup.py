5e815b1a0617a46f5338fda0901d2340ca4d9d41
Armaan Ahmed <aahmed22@scarsdaleschools.org>
2021-06-08 18:07:03 -0400

    changed 0.0.1 to 0.0.1-pre

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="taskmessenger",
-    version="0.0.1",
+    version="0.0.1-pre",
     author="Armaan Ahmed",
     author_email="aahmedresearch@gmail.com",
     description="Write emails after finishing a task",
